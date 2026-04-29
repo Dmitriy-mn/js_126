f49a69d742bfb902bd5a358d96b517adf8aa5501
Dmitriy-mn <dmitriy.k.89.mn@gmail.com>
2026-04-29 22:15:08 +0300

    lesson_22

---
+++ module_11/lesson_22/src/js/03-practice.js
@@ -0,0 +1,89 @@
+import axios from 'axios';
+
+import '../common.css';
+
+/**
+ * https://thecatapi.com/
+ *
+ * 1) Створити сторінку яка при завантаженні робить запит на бекенд і оримує
+ *  всі доступні breeds. https://api.thecatapi.com/v1/breeds
+ *
+ * 2) В інтерфейсі є форма пошуку з прив'язаним datalist з завантаженими breeds.
+ *
+ * 3) Користувач в формі вводить або обирає необхідний breed і при сабміті форми
+ *  виконується GET запит за зображенням.
+ *
+ * 4) Після запиту під формою відображається картка з зображенням.
+ *
+ * 5) Під час запиту під формую відображається loader
+ */
+
+
+const form = document.querySelector("#search-form");
+const breedsList = document.querySelector("#breeds-list");
+const breedInput = document.querySelector("#breed-input");
+const loader = document.querySelector("#loader");
+const catCard = document.querySelector("#cat-card");
+
+form.addEventListener("submit", handleSubmit);
+
+fetchBreeds();
+
+function fetchBreeds() {
+    axios("https://api.thecatapi.com/v1/breeds")
+        .then(({ data }) => {
+            breedsList.insertAdjacentHTML("beforeend", populateDatalist(data))
+        })
+        .catch(error => {
+            console.log(error.message)
+        })
+}
+
+function populateDatalist(arr) {
+    return arr.map(({ id, name }) => `
+        <option value="${name}" data-id="${id}"></option>
+    `).join("");
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    const myBreed = breedInput.value;
+    const selectedBreed = [...breedsList.options]
+        .find(item => item.value.toLowerCase() === myBreed.trim().toLowerCase());
+
+    if(!selectedBreed) {
+        alert("Оберіть існуючу породу");
+        return;
+    }
+    
+    const breedId = selectedBreed.dataset.id;
+    loader.classList.remove("hidden");
+
+    axios(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+        .then(({ data }) => {
+            if(data.length > 0) {
+                const catImg = data[0].url;
+
+                catCard.innerHTML = `
+                    <div class="card">
+                        <img src="${catImg}" alt="${myBreed}" class="card-image"/>
+                        <div class="card-body">
+                            <h2 class="card-title">${myBreed}</h2>
+                        </div>
+                    </div>
+                `
+            } else {
+                catCard.innerHTML = '<p class="error-text">Зображення не знайдено</p>'
+            }
+        })
+        .catch(error => {
+            alert(error.message);
+        })
+        .finally(() => {
+            event.target.reset();
+            loader.classList.add("hidden");
+        })
+}
+
+
